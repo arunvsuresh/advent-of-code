2aff8751831496855a0b9c6620426e9db74328f5
Arun Suresh <arunvsuresh@gmail.com>
2024-01-09 17:52:03 -0800

    refactor regex a bit more

--- day_1.py
+++ day_1.py
@@ -1,19 +1,18 @@
 import pandas as pd 
 import re
+import time
 
 # edge case: check to see if each calibration string has just one digit, if so sum that digit twice
 def sum_calibration_values(input_txt):
 
     df = pd.read_csv(input_txt, header=None, sep=" ")
-    calibration_values = df[0].tolist()
-    
-    outer_sum = 0
+    calibration_values = df[0].tolist()    
+    sum = 0
     for calibration_value in calibration_values:
-        inner_sum = 0
-        res = re.split(r'[a-z]*', calibration_value)
-        res = [chr for chr in res if len(chr) == 1 and chr.isdigit()]
-        inner_sum += int(res[0] + res[-1])
-        outer_sum += inner_sum
-    return outer_sum
-
+        matches = list(re.finditer(r'\d', calibration_value))
+        sum += int(matches[0].group() + matches[-1].group())
+    return sum
+start = time.time()
 print(sum_calibration_values("input.txt"))
+end = time.time()
+print('time to run...', end - start)
